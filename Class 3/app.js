769d9ad251c1414b1de2b1f54e833c00d4178d1f
unknown <abida.nizam11@gmail.com>
2021-05-29 15:32:14 +0500

    work on Global, local variable and for loop

---
+++ Class 3/app.js
@@ -0,0 +1,44 @@
+/*
+//var name = "abida";
+   console.log(name, 'name');
+
+
+
+
+function myFunction(){
+     var name = "abida";
+   // console.log(name, 'name');
+
+
+}
+ myFunction();
+ */
+
+/*console.log(game);
+
+ const game = "Crickrt";
+   //let game = "Football";
+   */
+
+
+   let games = ["Cricket", "Football", "Volley Ball","Basket Ball"];
+
+   
+   for (let i=0; i<=games.length; i++){
+       console.log(games[i], 'Members')
+   }
+
+
+      /*
+
+       let games = ["Cricket", "Football", "Volley Ball","Basket Ball"];
+
+     games.length=4;
+    games.length-1;
+
+     for (let i=games.length-1; i>=0; i--){
+       
+       console.log(games[i], 'Members')
+   }
+    
+    */
